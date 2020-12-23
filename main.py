f1dc0168909d7a323ca84010466de42d48b1978b
Christina Varghese <46170618+christinichka@users.noreply.github.com>
2020-12-23 08:44:16 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -29,7 +29,20 @@ while game_is_on:
 	#Detect collision with food. Calculate the distance between the snake head and the food.
 	if snake.head.distance(food) < 15:
 		food.refresh()
+		snake.extend()
 		scoreboard.increase_score()
 
-screen.exitonclick()
 
+	#Detect collison with wall.
+	if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+		game_is_on = False
+		scoreboard.game_over()
+
+	#Detect collision with own tail.
+	for segment in snake.segments[1:]:
+		if snake.head.distance(segment) < 10:
+			game_is_on = False
+			scoreboard.game_over()
+
+
+screen.exitonclick() 
