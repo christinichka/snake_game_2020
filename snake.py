0ccbf484a1d4315044c305af4f5ab1e0d0326130
Christina Varghese <46170618+christinichka@users.noreply.github.com>
2020-12-29 08:59:28 -0700

    Add files via upload

--- snake.py
+++ snake.py
@@ -26,13 +26,13 @@ class Snake:
 		new_segment.goto(position)
 		self.segments.append(new_segment)
 
-	def reset(self):
-		for seg in self.segments:
-			seg.goto(1000, 1000)
-
-		self.segments.clear()
-		self.create_snake()
-		self.head = self.segments[0]
+#	def reset(self):
+#		for seg in self.segments:
+#			seg.goto(1000, 1000)
+#
+#		self.segments.clear()
+#		self.create_snake()
+#		self.head = self.segments[0]
 
 	def extend(self):
 		#Use negative number to start counting from the end of the list.
